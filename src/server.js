7bfb3c54041e894e2a322a2077a5c8b1b5ffda67
WesleyMeneghini <wesley.meneghini@outlook.com.br>
2020-02-13 16:25:59 -0300

    Habilitando os methodos para as requisicoes

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.use(express.json())
 let allowCrossDomais = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*')
     next()
 }
 
